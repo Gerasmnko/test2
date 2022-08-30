19a51ee69293780f68d7f4751efaa5f7c97f999c
Gerasmnko <gerasmnko@gmail.com>
2022-08-30 22:09:35 +0300

    Merge pull request #2 from Gerasmnko/25.08.22
    Base components

--- src/components/Navi/Navi.js
+++ src/components/Navi/Navi.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./Navi.scss";
 
 const Navi = () => {
-  return <div className="navi"></div>;
+  return (
+    <div className="navi">
+      <a>Main</a>
+      <a>Messages</a>
+      <a>News</a>
+      <a>Settings</a>
+    </div>
+  );
 };
 
 export default Navi;
--- src/components/Wall/Wall.js
+++ src/components/Wall/Wall.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Wall.scss";
 
 const Wall = () => {
-  return <div className="wall"></div>;
+  return (
+    <div className="wall">
+      <div>
+        <img
+          className="logo"
+          src="http://cdn.cnn.com/cnnnext/dam/assets/181010131059-australia-best-beaches-cossies-beach-cocos3.jpg"
+        />
+      </div>
+      <div>Ava+description</div>
+      <div>Posts</div>
+      <div>
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Wall;
